f6c940ff2802fe1d687ca209d72c9be3d05ff356
wagner.dutra <wagnerdutra1010@gmail.com>
2019-08-30 18:22:01 -0300

    Adding project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { StyleSheet } from 'react-native';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+// const styles = StyleSheet.create({});
+
+export default App;
